fc25482cb175157da08e48105cd7ca0638d312cd
GuilhermefDomingues <guilhermefrancadomingues@outlook.com>
2025-02-26 11:05:24 -0300

    chore: create button component

---
+++ src/components/Button/Index.tsx
@@ -0,0 +1,10 @@
+import { TouchableOpacity, Text } from "react-native";
+import styles from "./styles";
+
+function Button(){
+    <TouchableOpacity style={styles.button}>
+        <Text style={styles.label}></Text>
+    </TouchableOpacity>
+}
+
+ export default Button;
